df1ee365847023ecf6065f7e7844cca1d191caa3
joaquim_silveira <joaquim.silveira@epfl.ch>
2021-04-18 13:41:40 -0700

    Analog, elec II, CdMII, SHS, ana num >>>>>>> ana IV

--- Projet/Projet/main.asm
+++ Projet/Projet/main.asm
@@ -1,32 +1,91 @@
 .include "lib/macros.asm"
 .include "lib/definitions.asm"
 
+
 reset:
-	LDSP	RAMEND
-	rcall	wire1_init
-	rcall	lcd_init
-	rjmp main
+		LDSP		RAMEND
+		rcall		wire1_init
+		rcall		lcd_init
+		rcall		encoder_init
+		ldi			a0, 0
+		jmp		intro
 
-.include "lib/lcd.asm"
 .include "lib/printf.asm"
-.include "lib/wire1.asm"
+.include "lib/lcd.asm"
+.include "lib/encoder.asm"
+.include "lib/wire1.asm"	
+.include "lib/menu.asm"	
 
-main:
-	rcall	wire1_reset
-	CA		wire1_write, skipROM
-	CA		wire1_write, convertT
-	WAIT_MS	750
+intro :
+		PRINTF	LCD
+.db	"Hello world", 0
+		WAIT_MS		1500
+		rjmp loop
+loop:
+	rcall			main_menu
+	rjmp			loop
 
-	rcall	lcd_home
-	rcall	wire1_reset
-	CA		wire1_write, skipROM
-	CA		wire1_write, readScratchpad
-	rcall	wire1_read
-	mov		c0,a0
-	rcall	wire1_read
-	mov		a1, a0
-	mov		a0, c0
+	
+main_menu:
+		rcall		encoder
+		brts		mes_choice			
+		CYCLIC		a0,0,2
+		PRINTF		LCD
+.db		CR, CR, FHEX,a,0
+		rcall		menui
+.db		"Temperature|Humidity   |Light      ",0		
+		WAIT_MS		1
+		rjmp		main_menu
 
-	PRINTF	LCD
-.db	"temp=",FFRAC2+FSIGN,a,4,$42,"C ", CR,0
-	rjmp	main
+mes_choice:				; switch case selon le choix du menu (-> a0) pour l'affichage de la mesure
+		ldi			w, 0x0000		;Temperature code
+		cp			a0,w
+		breq		temperature		
+		ldi			w, 0x0001		;Humidity code
+		cp			a0,w
+		breq		humidity
+		rjmp		mes_choice
+
+temperature:
+	rcall		wire1_reset
+	CA			wire1_write, skipROM
+	CA			wire1_write, convertT
+	WAIT_MS		1
+	rcall		lcd_home
+	rcall		wire1_reset
+	CA			wire1_write, skipROM
+	CA			wire1_write, readScratchpad
+	rcall		wire1_read
+	mov			c0,a0
+	rcall		wire1_read
+	mov			a1, a0
+	mov			a0, c0
+	PRINTF		LCD
+.db	"temp=",FFRAC2+FSIGN,a,4,$42,"C  ",CR,0
+	rcall		encoder
+	brts		come_back
+	rjmp		temperature
+
+humidity:
+	PRINTF		LCD
+.db	"humidity=",FFRAC2+FSIGN,a,4,$42,"per cent",CR,0
+	rcall		encoder
+	brts		come_back
+	rjmp		humidity
+
+light:
+	PRINTF		LCD
+.db	"light=",FFRAC2+FSIGN,a,4,$42,"lm  ",CR,0
+	rcall		encoder
+	brts		come_back
+	rjmp		light
+
+
+come_back:
+	ldi			a0, 0
+	rjmp		loop
+
+	
+	
+
+
--- Projet/Projet/sensors1.asm
+++ Projet/Projet/sensors1.asm
@@ -1,25 +1,9 @@
 
-;.include "sensors1.asm"
-
-main_menu:
-		rcall		encoder
-		brts		temperature
-		CYCLIC		a0,0,2
-		PRINTF		LCD
-.db		CR, CR, FHEX,a,0
-		rcall		menui
-.db		"Temperature|Humidity   |Light      ",0		
-		WAIT_MS		1
-		rjmp		main_menu
-
-
 temperature:
-	rcall		encoder
-	brts		main_menu
 	rcall		wire1_reset
 	CA			wire1_write, skipROM
 	CA			wire1_write, convertT
-	WAIT_MS		1
+	WAIT_MS		500
 	rcall		lcd_home
 	rcall		wire1_reset
 	CA			wire1_write, skipROM
@@ -27,11 +11,10 @@ temperature:
 	rcall		wire1_read
 	mov			c0,a0
 	rcall		wire1_read
-	mov			a1, a0
-	mov			a0, c0
+	mov			a2, a1
+	mov			a1, c0
 	PRINTF		LCD
 .db	"temp=",FFRAC2+FSIGN,a,4,$42,"C  ",CR,0
 	rjmp		temperature
 	
-
 
